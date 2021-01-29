83e9536adb01b614613d3a47a90accc518113364
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-01-29 09:24:11 +0100

    Modifico la clase colaborador

--- src/dam/programacion/ColaboradorClass.java
+++ src/dam/programacion/ColaboradorClass.java
@@ -3,5 +3,6 @@ package dam.programacion;
 public class ColaboradorClass {
     public void funcion(){
         System.out.println("Probando la clase colaborador");
+        System.out.println("Clase colaborador funcionando correctamente");
     }
 }
